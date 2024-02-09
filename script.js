53b2615c0333765ce2ba6c19724a0ce1537b4b1f
alexajaime09 <alexaquezada23@gmail.com>
2024-02-09 17:20:27 -0600

    2nd update

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let darkmode = document.querySelector('#darkmode');
 
-darkmode.onclick = () = > {
-
+darkmode.onclick = () => {
+if (darkmode.classList.contains('bx bx-moon'))
+{
+    darkmode.classList.replace('bx bx-moon', 'bx bx-sun');
+    document.body.classList.add('color');
+}
+else{
+    darkmode.classList.replace('bx bx-sun', 'bx bx-moon');
+    document.body.classList.remove('color');
+}
 }
